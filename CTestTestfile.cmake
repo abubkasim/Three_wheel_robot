952a36f6aafc827656061642632e27dfef26b9bc
Abuki@ethiodig <78879750+abubkasim@users.noreply.github.com>
2024-07-09 18:28:03 +0300

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/abuki/3wheel-robot/src
+# Build directory: /home/abuki/3wheel-robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_dd_model_pkg")
